1c0734c7a5767976df9feac916732c1d9fc4ea81
Oskar <gustosk@gmail.com>
2020-11-06 18:19:25 +0200

    created login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,67 +1,84 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
+import {
+  Button,
+  Icon,
+  Segment,
+  Grid,
+  Divider,
+  Header,
+} from "semantic-ui-react";
 
 const Login = () => {
   return (
-    <>
-      <h2>Login</h2>
-
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.username) {
-            errors.username = "Käyttäjänimi puuttuu";
-          } else if (values.username.length < 4) {
-            errors.username = "Käyttäjänimi liian lyhyt";
-          }
-          if (!values.password) {
-            errors.password = "Salasana puuttuu";
-          } else if (values.password.length < 8) {
-            errors.password = "Salasana liian lyhyt";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,          
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="username"
-              name="username"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.username}
-            />
-            {errors.username && touched.username && errors.username}
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {errors.password && touched.password && errors.password}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
-    </>
+    <Segment placeholder>
+      <Grid columns={2} relaxed="very" stackable>
+        <Grid.Column>
+          <div className='form'>     
+            <Header as="h1">Login</Header>
+            <Formik
+              initialValues={{ username: "", password: "" }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.username) {
+                  errors.username = "Käyttäjänimi puuttuu";
+                } else if (values.username.length < 4) {
+                  errors.username = "Käyttäjänimi liian lyhyt";
+                }
+                if (!values.password) {
+                  errors.password = "Salasana puuttuu";
+                } else if (values.password.length < 8) {
+                  errors.password = "Salasana liian lyhyt";
+                }
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                alert(JSON.stringify(values));
+                setSubmitting(false);
+              }}
+            >
+              {({ errors, touched, isSubmitting }) => (
+                <Form>
+                  <div className="inputField">
+                    <label htmlFor="username">Käyttäjänimi</label>
+                    <Field name="username" placeholder="käyttäjänimi" />
+                    {errors.username && touched.username ? (
+                      <div>{errors.username}</div>
+                    ) : null}
+                  </div>
+                  <div className="inputField">
+                    <label htmlFor="password">Salasana</label>
+                    <Field
+                      type="password"
+                      name="password"
+                      placeholder="salasana"
+                    />
+                    {errors.password && touched.password ? (
+                      <div>{errors.password}</div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <Button
+                      animated
+                      type="submit"
+                      disabled={isSubmitting}
+                      color="green"
+                    >
+                      <Button.Content visible>Kirjaudu</Button.Content>
+                      <Button.Content hidden>
+                        <Icon name="arrow right" />
+                      </Button.Content>
+                    </Button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </Grid.Column>
+        <Grid.Column>test</Grid.Column>
+      </Grid>
+      <Divider vertical>-</Divider>
+    </Segment>
   );
 };
 
