45254062578c676cd5e2d0d98294802f696c079d
Oskar <gustosk@gmail.com>
2020-11-07 19:28:12 +0200

    loginpage design ready

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,83 +1,85 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik } from "formik";
+import * as Yup from 'yup';
 import {
+  Form,
   Button,
   Icon,
   Segment,
-  Grid,
-  Divider,
+  Grid,  
   Header,
+  Label
 } from "semantic-ui-react";
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+  .min(4, 'Käyttäjänimi liian lyhyt')
+  .max(15, 'Käyttäjänimi liian pitkä')
+  .required('Käyttäjänimi puuttuu'),
+  password: Yup.string()
+  .min(8, 'Salasana liian lyhyt')
+  .required('Salasana puuttuu')
+})
 
 const Login = () => {
   return (
     <Segment placeholder>
-      <Grid columns={2} relaxed="very" stackable>
+      <Grid columns={1} relaxed="very" stackable>
         <Grid.Column>
-          <div className='form'>     
+          <div className="form">
             <Header as="h1">Login</Header>
             <Formik
               initialValues={{ username: "", password: "" }}
-              validate={(values) => {
-                const errors = {};
-                if (!values.username) {
-                  errors.username = "Käyttäjänimi puuttuu";
-                } else if (values.username.length < 4) {
-                  errors.username = "Käyttäjänimi liian lyhyt";
-                }
-                if (!values.password) {
-                  errors.password = "Salasana puuttuu";
-                } else if (values.password.length < 8) {
-                  errors.password = "Salasana liian lyhyt";
-                }
-                return errors;
-              }}
+              validationSchema={validationSchema}
               onSubmit={(values, { setSubmitting }) => {
                 alert(JSON.stringify(values));
                 setSubmitting(false);
               }}
             >
-              {({ errors, touched, isSubmitting }) => (
-                <Form>
-                  <div className="inputField">
-                    <label htmlFor="username">Käyttäjänimi</label>
-                    <Field name="username" placeholder="käyttäjänimi" />
-                    {errors.username && touched.username ? (
-                      <div>{errors.username}</div>
-                    ) : null}
-                  </div>
-                  <div className="inputField">
-                    <label htmlFor="password">Salasana</label>
-                    <Field
-                      type="password"
-                      name="password"
-                      placeholder="salasana"
+              {(props) => (
+                <Form onSubmit={props.handleSubmit}>                  
+                    <Form.Input
+                      type="text"
+                      onChange={props.handleChange}
+                      value={props.values.username}
+                      name="username"
+                      icon="user"
+                      iconPosition="left"
+                      label="Käyttäjänimi"
+                      placeholder="käyttäjänimi"
                     />
-                    {errors.password && touched.password ? (
-                      <div>{errors.password}</div>
-                    ) : null}
-                  </div>
-                  <div>
-                    <Button
-                      animated
-                      type="submit"
-                      disabled={isSubmitting}
-                      color="green"
-                    >
-                      <Button.Content visible>Kirjaudu</Button.Content>
-                      <Button.Content hidden>
-                        <Icon name="arrow right" />
-                      </Button.Content>
-                    </Button>
-                  </div>
+                    {props.errors.username && <Label pointing color='yellow'>{props.errors.username}</Label>}
+                  
+                  <Form.Input
+                    type="password"
+                    onChange={props.handleChange}
+                    value={props.values.password}
+                    name="password"
+                    icon="lock"
+                    iconPosition="left"
+                    label="Salasana"
+                    placeholder="salasana"
+                  />
+                  {props.errors.password && <Label pointing color='yellow'>{props.errors.password}</Label>}
+                  <Button
+                    animated
+                    type="submit"
+                    disabled={props.isSubmitting}
+                    color="green"
+                  >
+                    <Button.Content visible>Kirjaudu</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="arrow right" />
+                    </Button.Content>
+                  </Button>
                 </Form>
               )}
             </Formik>
           </div>
         </Grid.Column>
-        <Grid.Column>test</Grid.Column>
+        
       </Grid>
-      <Divider vertical>-</Divider>
+      
     </Segment>
   );
 };
